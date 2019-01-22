98d9b54ec00d86ec56861160e150bf3a8cf94267
Epadilla1886 <ejpadilla1886@gmail.com>
2019-01-21 18:28:33 -0800

    JS | AJAX - EJ Padilla

--- js_ajax/main.js
+++ js_ajax/main.js
@@ -1,6 +1,6 @@
 //Create GLOBAL variable below here on line 2
 
-
+var global_result;
 
 $(document).ready(initializeApp);
 
@@ -13,15 +13,28 @@ function getData(){
     var ajaxConfig = {
         dataType: 'json',
         url: 'http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topMovies/json',
-        success: function(result) {
-            
+        success: function (result) {
+            global_result = result;
             console.log('2) AJAX Success function called, with the following result:', result);
+            var firstMovieInfo = global_result.feed.entry[0]['im:image'][2]; // 3rd image of first movie
 
+            for (var index = 0; index < 10; index++) {
+                var movieImage = global_result.feed.entry[index]['im:image'][2].label;
+                var movieTitle = global_result.feed.entry[index]['im:name'].label;
+                var movieDirector = global_result.feed.entry[index]['im:artist'].label;
+
+                    $('#main').append($('<img>').attr('src',movieImage));
+                    $('#main').append($('<div>').text(movieTitle));
+                    $('#main').append($('<div>').text(movieDirector));
+                }
+            }
         }
-    }
+
+
 
     console.log('3) Making AJAX request');
     $.ajax(ajaxConfig);
 
     console.log('4) End of getData');
+
 }
